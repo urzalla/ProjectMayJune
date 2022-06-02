62a88ef990dd9bd9a4b0ce9e1bb47dbc63d6d457
Urzalla Razmowar <urzallarazmowar@Urzallas-MacBook-Air.local>
2022-06-01 20:07:49 -0400

    ProjectMay

---
+++ src/github/gitAndGithub.java
@@ -0,0 +1,9 @@
+package github;
+
+public class gitAndGithub {
+
+	public static void main(String[] args) {
+		System.out.println("today we learn github");
+		System.out.println("today is a good day");
+	}
+}
---
+++ src/github/package-info.java
@@ -0,0 +1,9 @@
+//		git class03
+
+
+// Tracking out project in git
+
+// 1. Navigate our terminal to the location where we want to create 
+// the repository..
+// 2. initialize the repository
+// 3. 
